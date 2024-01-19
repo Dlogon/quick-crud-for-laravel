be53605ff41bd88492d0c871fbf024cbca4000fc
Diego <dlogon@outlook.com>
2024-01-18 18:04:55 -0600

    Add search helper test

---
+++ tests/Feature/SearchClassTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Dlogon\QuickCrudForLaravel\Tests\Feature;
+
+use Dlogon\QuickCrudForLaravel\Helpers\Search;
+use Dlogon\QuickCrudForLaravel\Models\Blog;
+use Dlogon\QuickCrudForLaravel\Tests\TestCase;
+use Illuminate\Http\Request;
+
+class SearchClassTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+    }
+    public function test_that_search_find_model_by_like(): void
+    {
+        //given the search class and the query params
+
+        $queryParams = [
+            "name" => \json_encode([
+            "type" => "text",
+            "value" => "dlogon"
+            ]),
+        ];
+
+        //when search a model with type text and name dlogon
+
+        $models = Search::searchByQueryParamsWithArray((new Blog)::query(), $queryParams);
+        //then assert that you find a query with a model with id 11 and content Hello from quick-crud
+        //dd(Blog::all()->toArray());
+        $blog = $models->first();
+        $this->assertSame(11, $blog->id);
+        $this->assertSame("Hello from quick-crud", $blog->content);
+    }
+}
---
+++ tests/Unit/SearchClassTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Dlogon\QuickCrudForLaravel\Tests\Unit;
+
+use Dlogon\QuickCrudForLaravel\Helpers\Search;
+use Dlogon\QuickCrudForLaravel\Models\Blog;
+use Dlogon\QuickCrudForLaravel\Tests\TestCase;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+class SearchClassTest extends TestCase
+{
+    private $queryParams;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+    }
+    public function test_that_request_call_has(): void
+    {
+        $request = $this->createMock(Request::class);
+
+        $request->expects($this->once())->method("has");
+
+        Search::searchByQueryParams(new Blog, $request);
+    }
+
+    public function test_that_query_calls_wheres(): void
+    {
+        $queryParams = [
+            "name" => \json_encode([
+            "type" => "text",
+            "value" => "dlogon"
+            ]),
+            "content" => \json_encode([
+            "type" => "related",
+            "value" => "s"
+            ]),
+            "created_at" => \json_encode([
+            "type" => "single-date",
+            "value" => "s"
+            ]),
+        ];
+        $query = $this->createMock((new Blog())::query()::class);
+        $query->expects($this->exactly(2))->method("where");
+        //$query->expects($this->once())->method("whereDate");
+
+
+        Search::searchByQueryParamsWithArray($query, $queryParams);
+    }
+
+
+}
